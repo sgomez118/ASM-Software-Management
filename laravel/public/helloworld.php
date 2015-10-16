0de752c2cc52ef2de6aaebe3ff79580d267e7e05
Andre Chen <ataychen@gmail.com>
2015-10-16 14:34:25 -0700

    initial laravel commit

---
+++ laravel/public/helloworld.php
@@ -0,0 +1,11 @@
+
+<html>
+    <head>
+        <title> PHP Test </title>
+    </head>
+    <body>
+        <?php echo '<p> Hello World </p>'; /* will be different if "? php" */ ?> 
+    </body>
+</html>
+
+
