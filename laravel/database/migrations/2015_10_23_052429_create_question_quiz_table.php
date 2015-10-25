752ad4a438465d3c3a34a1ef29aabe394c608d3b
Misha Dowd <mishadow@usc.edu>
2015-10-24 20:49:41 -0700

    Adding question_quiz migration

---
+++ laravel/database/migrations/2015_10_23_052429_create_question_quiz_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQuestionQuizTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('question_quiz', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('quiz_id')->unsigned();
+            $table->integer('question_id')->unsigned();
+            //Defining Foreign Relationships
+            $table->foreign('quiz_id')
+                    ->references('id')->on('quizes');
+            $table->foreign('question_id')
+                    ->references('id')->on('questions');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('question_quiz');
+    }
+}
