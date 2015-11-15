c0e4404f8ae74c8546c09f778aa9542069fd764a
Misha N Dowd <Misha.N.Dowd@gmail.com>
2015-11-14 17:04:04 -0800

    Removed 'use SoftDeletes' from answer

--- laravel/app/Answer.php
+++ laravel/app/Answer.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Answer extends Model
 {
     protected $fillable = ['text', 'image'];
-    use SoftDeletes;
+    
 
     public function questions(){
     	return $this->belongsToMany('App\Question')->withPivot('is_correct');
---
+++ questionAdd.sql
@@ -0,0 +1,17 @@
+insert into aqsDB.questions (prompt, difficulty, created_at, updated_at, type, subject_id) 
+values ('Will this question be added with the help of the controller?', 'hard', '2015-10-30 06:19:18', '2015-10-30 06:19:18', 'multi-value', 1);
+
+insert into aqsDB.questions (prompt, difficulty, created_at, updated_at, type, subject_id) 
+values ('The simplest way to search an array is called ... ', 'easy', '2015-11-02 06:10:35', '2015-11-02 06:10:35', 'multi-value', 1);
+
+insert into aqsDB.questions (prompt, difficulty, created_at, updated_at, type, subject_id) 
+values ('Arrange the following complexities in increasing order: Quadratic, constant, exponential, logarithmic', 'medium', '2015-11-02 06:11:50', '2015-11-02 06:11:50', 'multi-value', 1);
+
+insert into aqsDB.questions (prompt, difficulty, created_at, updated_at, type, subject_id) 
+values ('Suppose a sorting function “f” is O(n log n) where n is the number of elements in an array.  Select all true statements: ', 'medium', '2015-11-02 06:24:03', '2015-11-02 06:24:03', 'multi-value', 1);
+
+insert into aqsDB.questions (prompt, difficulty, created_at, updated_at, type, subject_id) 
+values ('Suppose functions real-valued functions “f” and “g” (with domain all real numbers) are both O(log n).  Which of the following statements must be true? ', 'medium', '2015-11-02 06:26:36', '2015-11-02 06:26:36', 'multi-value', 1);
+
+insert into aqsDB.questions (prompt, difficulty, created_at, updated_at, type, subject_id) 
+values ('What is the minimum time complexity of insertion and deletion of data using a Stack, where the item for deletion is at the top of the Stack? ', 'easy', '2015-11-02 06:27:21', '2015-11-02 06:27:21','multi-value', 1);
